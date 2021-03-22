067e0f8bf697fe1a4dad7d4ba6e71a907cdbcf60
904154001 <904154001@L829WD150098.edu.cbe.ab.ca>
2021-03-22 12:58:53 -0600

    working on it

--- TriangleClassifier/src/TriangleClassifier.java
+++ TriangleClassifier/src/TriangleClassifier.java
@@ -8,4 +8,33 @@ public class TriangleClassifier {
 		}
 		return valid_triangle;
 	}
+	public static String classifyByAngle(int a, int b, int c) {
+		String triangleAngle;
+		if (a < 90 && b < 90 && c < 90) {
+			triangleAngle = "acute";
+		}
+		else if (a == 90 || b == 90 || c == 90) {
+			triangleAngle = "right";
+		}
+		else {
+			triangleAngle = "obtuse";
+		}
+		return triangleAngle;
+	}
+
+	public static String classifyBySide(int a, int b, int c) {
+		String triangleSide;
+		if (a == b && b == c) {
+			triangleSide = "equilateral";
+		}
+		else if (a != b && b != c && a != c) {
+			triangleSide = "scalene";
+		}
+		else {
+			triangleSide = "isosceles";
+		}
+		return triangleSide;
+	}
+	
 }
+
