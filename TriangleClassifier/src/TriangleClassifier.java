f3708efb364c1014c7ff37e8f3600793d5881460
jchid <jchid@192.168.1.79>
2021-03-21 19:49:21 -0600

    valid triangle done

--- TriangleClassifier/src/TriangleClassifier.java
+++ TriangleClassifier/src/TriangleClassifier.java
@@ -3,6 +3,9 @@ public class TriangleClassifier {
 
 	public static boolean isValidTriangle(int a, int b, int c) {
 		boolean valid_triangle = false;
-		
+		if (a + b + c == 180 && a > 0 && b > 0 && c > 0) {
+			valid_triangle = true;
+		}
+		return valid_triangle;
 	}
 }
